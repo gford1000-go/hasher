4ec56e25df65446faf83d033ba8c25b154c3d9a5
Geoff Ford <gford1000@hotmail.com>
2020-05-04 11:43:50 +0100

    Added commentary

--- example_test.go
+++ example_test.go
@@ -19,7 +19,7 @@ func Example() {
 		C: "Hello",
 	}
 
-	h, _ := Hash(v)
+	h, _ := Hash(v) // Illustrates hashing arbitrary objects
 	fmt.Println(h)
 	// Output: [74 175 82 242 158 82 154 251 165 204 11 169 52 189 125 83 245 147 182 197 53 143 167 187 38 128 108 203 210 240 5 226]
 }
